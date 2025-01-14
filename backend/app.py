0143c3800d4c735618950f1636a5e7c72b64112b
Michael Valdez <mikeyvaldez92@yahoo.com>
2025-01-14 15:36:56 -0800

    getting ready for deployment

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+# This file needs to be updated before deployment
+
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -11,6 +14,19 @@ app.config["SQLACHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+frontend_folder = os.path.join(os.getcwd(), "..","frontend")
+dist_folder = os.path.join(frontend_folder,"dist")
+
+# Server static files from the "dist" folder under the frontend directory
+@app.route("/",defaults={"filename":""})
+@app.route("/<path:filename>")
+def index(filename):
+    if not filename:
+        filename = "index.html"
+    return send_from_directory(dist_folder,filename)
+
+
+# api routes
 import routes
 
 with app.app_context():
