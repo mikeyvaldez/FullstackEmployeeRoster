e7497b8ab912e42e9efb0979813be0576c005388
Michael Valdez <mikeyvaldez92@yahoo.com>
2025-01-14 16:18:40 -0800

    edit to requirements file

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -2,3 +2,7 @@ from app import app
 
 if __name__ == "__main__":
     app.run()
+
+
+# Gunicorn and WSGI (Web Server Gateway Interface) are both components used in deploying and serving Python Web Applications, particularily those built with
+# web frameworks like Flask and Django.
