03ee3f6aa2a5a26fe8fb6670460b65b0b638c4fb
Michael Valdez <mikeyvaldez92@yahoo.com>
2025-01-16 11:19:05 -0800

    edit to db

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,8 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///employees.db"
+
 app.config["SQLACHEMY_TRACK_MODIFICATIONS"] = False
 
 
