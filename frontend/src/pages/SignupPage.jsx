347cebff59f2655ab7a2be3d8fd7f185c6084939
Michael Valdez <mikeyvaldez92@yahoo.com>
2025-02-06 16:45:33 -0800

    added new pages

--- frontend/src/pages/SignupPage.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function SignupPage() {
-  return (
-    <div>SignupPage</div>
-  )
-}
-// Employees picture link
-// https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F41%2F17%2F73%2F411773e40b4057645a51293872dcfd0c.jpg&f=1&nofb=1&ipt=1d833a6c006ddf363b7699c872ee7d921a0708504f0c5be2b527446fe39a5a15&ipo=images
