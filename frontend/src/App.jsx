c291f714740b70019fe091bd27cbe9e2fce13ce9
Michael Valdez <mikeyvaldez92@yahoo.com>
2025-02-06 16:42:15 -0800

    added new app and main files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,25 @@
-import { Stack, Container, Text } from "@chakra-ui/react";
-import Navbar from "./components/Navbar.jsx";
-import UserGrid from "./components/EmployeeGrid.jsx";
-import { useState } from "react";
+import { UserProvider } from "./context/userContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import ManagePage from "./pages/ManagePage";
+import RegistrationPage from "./pages/RegistrationPage";
+import ProtectedRoute from './components/ProtectedRoute'; // Import ProtectedRoute
 
-function App() {
-  // if we were using something like react context or redux we would place this state
-  // in the user grid only.
-  // but since it is a smaller app we pass the setEmployees to navbar and usergrid, to keep things simple
-  const [employees, setEmployees] = useState([]);
-
-
+export default function App() {
   return (
-    <Stack minH={"100vh"}>
-      <Navbar setEmployees={setEmployees} />
-
-      <Container maxW={"1200px"} my={4}>
-        <Text
-          fontSize={{ base: "3xl", md: "50" }}
-          fontWeight={"bold"}
-          letterSpacing={"2px"}
-          textTransform={"uppercase"}
-          textAlign={"center"}
-          mb={8}
-          color={"green.500"}
-        >
-          My Employees
-        </Text>
-
-        <UserGrid employees={employees} setEmployees={setEmployees}/>
-      </Container>
-    </Stack>
+    <UserProvider>
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/registration' element={<RegistrationPage />} />
+          
+          <Route element={<ProtectedRoute />}>
+            <Route path="/manage" element={<ManagePage />} />
+          </Route>          
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
   );
 }
-
-export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import { ChakraProvider } from "@chakra-ui/react";
-import { ColorModeProvider } from "@chakra-ui/color-mode";
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 
-createRoot(document.getElementById("root")).render(
+
+createRoot(document.getElementById('root')).render(
   <ColorModeProvider>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </ColorModeProvider>
-);
+  </ColorModeProvider>  
+)
