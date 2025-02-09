99f2b9a71123a5c36312754e8dcc13aa87eff8dc
Michael Valdez <mikeyvaldez92@yahoo.com>
2025-02-09 13:39:47 -0800

    edit to docker and wsgi

--- Dockerfile
+++ Dockerfile
@@ -11,3 +11,10 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 # Copy the rest of the files
 COPY backend/ /app
+
+
+# Expose the port that Flask runs on
+EXPOSE 5000
+
+# Command to run the Flask app
+CMD ["gunicorn", "-b", "0.0.0.0:5000", "app:app"]
--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,7 +1,7 @@
 from app import app
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
 
 
 # Gunicorn and WSGI (Web Server Gateway Interface) are both components used in deploying and serving Python Web Applications, particularily those built with
